bc3dd5f5fe83351c84416f419381ced201465e2e
Laurenz <ddanilenko350@gmail.com>
2025-09-13 19:32:26 +0300

    A handler for back to main menu added.

--- app/bot/start.py
+++ app/bot/start.py
@@ -1,8 +1,8 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-from app.core import logger
+from app.core import logger, get_user_info
 from app.bot import build_main_menu
 
 router = Router()
@@ -20,4 +20,18 @@ async def cmd_start(message: Message) -> None:
     
     logger.info(
         f"User {username}({user_id}) started the bot."
+    )
+    
+    
+@router.message(F.text == "🔙Back to main menu")
+async def cmd_back_to_main_menu(message: Message) -> None:
+    user_id, username, first_name, last_name = await get_user_info(message)
+    
+    await message.answer(
+        'Returning to main menu...',
+        reply_markup=await build_main_menu()
+    )
+    
+    logger.info(
+        f'User {username}({user_id}) returned to main menu'
     )
